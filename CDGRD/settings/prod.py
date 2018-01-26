7b151451c306c33732e55fbc7d5ffa47f8498125
Carlos Del Real <carloshdelreal@gmail.com>
2018-01-26 15:50:31 -0500

    Setting up the database

--- CDGRD/settings/prod.py
+++ CDGRD/settings/prod.py
@@ -1,2 +1,15 @@
 from CDGRD.settings.base import *
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carloshdelreal$consejeria',
+        'USER': 'carloshdelreal',
+        'PASSWORD': 'riesgo2016',
+      
+    }
+}
