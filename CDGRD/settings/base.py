140b0dcacb022e65478aaac1e93b7ae557975549
Carlos Del Real <carloshdelreal@gmail.com>
2018-04-02 07:59:57 -0500

    Crispy forms Configurated

--- CDGRD/settings/base.py
+++ CDGRD/settings/base.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Repmuni',
     'home',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
